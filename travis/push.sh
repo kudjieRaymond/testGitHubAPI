81ca8d4ae9406eaef0ba8332432b02d8353ba848
benoitgaudou <benoit.gaudou@gmail.com>
2020-05-31 19:12:20 +0700

    update travis

--- travis/push.sh
+++ travis/push.sh
@@ -2,8 +2,8 @@ git config --global user.email "benoit.gaudou@gmail.com"
 git config --global user.name "benoitgaudou"
 git config --global push.default simple
 git status
-
-git remote rm origin
-git remote add origin https://benoitgaudou:$GH_token_public_repo@github.com/benoitgaudou/testGitHubAPI.git
+#git remote rm origin
+#git remote add origin https://benoitgaudou:$GH_token_public_repo@github.com/benoitgaudou/testGitHubAPI.git
+#git commit ./data/* --message "update data"
 git status
-git push origin HEAD:master
+#git push origin HEAD:master
