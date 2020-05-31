ae90537efaff1f1214ac9c4a9b17e366892bcb0d
benoitgaudou <benoit.gaudou@gmail.com>
2020-05-31 16:38:45 +0700

    add push script and update git

---
+++ travis/push.sh
@@ -0,0 +1,9 @@
+git config --global user.email "benoit.gaudou@gmail.com"
+git config --global user.name "benoitgaudou"
+git config --global push.default simple
+git status
+
+git remote rm origin
+git remote add origin https://benoitgaudou:$GH_token_public_repo@github.com/benoitgaudou/testGitHubAPI.git
+git status
+git push origin HEAD:master
