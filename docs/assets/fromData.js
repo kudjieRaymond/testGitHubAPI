2c068c03cad2c70aadd603a7add2115767bd797e
Ray Kudjie <rkudjie@gmail.com>
2021-06-08 13:46:43 +0200

    fix bug

--- docs/assets/fromData.js
+++ docs/assets/fromData.js
@@ -3,11 +3,12 @@ function displayData(url, elt) {
 	req.open("GET", url, false);
 	req.send(null);
 	var dataJSON = JSON.parse(req.responseText);
-	var labels = [];
-	var data = [];
-	var canvaId = elt.id + "-canva";
 
 	dataJSON.versions.forEach(function (version) {
+		var labels = [];
+		var data = [];
+		var canvaId = elt.getAttribute("id") + "-version-" + ++i;
+
 		var sublist = "";
 		version.allOS.forEach(function (os) {
 			sublist =
@@ -35,26 +36,25 @@ function displayData(url, elt) {
 			'<ul><details><summary style="display: list-item;">Details</summary>' +
 			sublist +
 			"</details></ul>";
-	});
 
-	var ctx = document.getElementById(canvaId).getContext("2d");
-	var myChart = new Chart(ctx, {
-		type: "line",
-		data: {
-			labels: labels,
-			datasets: [
-				{
-					label: "# of Downloads",
-					data: data,
-					borderWidth: 1,
-					fill: false,
-					borderColor: "rgb(75, 192, 192)",
-					tension: 0.1,
-				},
-			],
-		},
+		var ctx = document.getElementById(canvaId).getContext("2d");
+		var myChart = new Chart(ctx, {
+			type: "line",
+			data: {
+				labels: labels,
+				datasets: [
+					{
+						label: "# of Downloads",
+						data: data,
+						borderWidth: 1,
+						fill: false,
+						borderColor: "rgb(75, 192, 192)",
+						tension: 0.1,
+					},
+				],
+			},
+		});
 	});
-
 }
 
 var eltGAMAhisto = document.getElementById("histo-gama");
